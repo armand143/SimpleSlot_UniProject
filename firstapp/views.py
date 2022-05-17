b9cb0bf3f16dccc461d3638590533f46b4319d2d
FedericaFass <104429760+FedericaFass@users.noreply.github.com>
2022-05-17 17:53:07 +0200

    Update views.py

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,13 +1,17 @@
+from multiprocessing import context
+from tkinter import Entry
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 # Create your views here.
 from .models import Todo
-
+from .forms import TodoForm
 
 def homepage(request):
-    todos = Todo.objects.all().order_by('deadline')
-    return render(request,'firstapp\Übersicht.html', {'todos':todos})
+    todos = Todo.objects.all()
+    context ={'todos' : todos}
+    return render(request,'firstapp/Übersicht.html', context)
 
 def index(request):
     return HttpResponse("It started correctly, nice")
@@ -17,7 +21,16 @@ def edit(request, todo_id):
     return render(request, 'firstapp/EditTODO.html', {'todo.title' : elem.title}, {'todo.deadline' : elem.deadline}, {'todo.percent' : elem.percent})
 
 def new(request):
-    return render(request, 'firstapp/NewTODO.html')
+    submitted = False
+    if request.method =="POST":
+        form = TodoForm(request.POST)
+        form.save()
+        return HttpResponseRedirect('/New submitted=True')
+    else:
+      form = TodoForm()
+      if 'submitted' in request.GET:
+          submitted = True
+    return render(request, 'firstapp/NewTODO.html', {'form' : form, 'submitted' : submitted})
 
 def impressum(request):
     return render(request, 'firstapp/Impressum.html')
