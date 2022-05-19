f3bc45ef81b5061f5bd0469146a006bd60e1263b
V-Shiro <104509168+V-Shiro@users.noreply.github.com>
2022-05-19 16:04:42 +0200

    fixed new action

--- firstapp/models.py
+++ firstapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=250)
-    deadline = models.DateTimeField()
+    deadline = models.DateField()
     percent = models.PositiveIntegerField(default=0)
 
     def __str__(self):
