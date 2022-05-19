eaf306920dc8a70b80b05fbb3909423fd6172503
V-Shiro <104509168+V-Shiro@users.noreply.github.com>
2022-05-19 19:44:31 +0200

    added max to percent with validator

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=250)
     deadline = models.DateField()
-    percent = models.PositiveIntegerField(default=0)
+    percent = models.PositiveIntegerField(default=0,
+                validators=[MaxValueValidator(100),MinValueValidator(0)])
 
     def __str__(self):
         return self.id
