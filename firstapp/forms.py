7f2f436d83d6b99aade9510a7603ce784ac342a3
FedericaFass <104429760+FedericaFass@users.noreply.github.com>
2022-06-01 14:14:35 +0200

    Update forms.py

--- firstapp/forms.py
+++ firstapp/forms.py
@@ -1,9 +1,15 @@
 from dataclasses import fields
 from django import forms
 from django.forms import ModelForm
-from .models import Todo
-
+from .models import Cluster, Todo
+        
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
         fields = ('title', 'deadline', 'percent')
+
+        
+class ClusterForm(ModelForm):
+    class Meta:
+        model = Cluster
+        fields = ('title', 'quantity', 'duration', 'availability')
