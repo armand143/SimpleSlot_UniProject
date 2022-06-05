f2e7484c3c0135f651cc4e6101dc9a1b6d50539f
FedericaFass <104429760+FedericaFass@users.noreply.github.com>
2022-06-05 13:12:19 +0200

    Update urls.py

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from . import views
-
 urlpatterns = [
-    path('', views.homepage, name='Übersicht'),
+    path('homepage/', views.homepage, name='Übersicht'),
     path('impressum/', views.impressum, name='Impressum'),
     path('new/', views.new, name='New'),
     path('edit/<cluster_id>', views.edit, name='edit'),
     path('delete/<todo_id>', views.delete, name='delete'),
     path('cls/', views.homepagestudis, name='homestudi'),
+    path('', views.login, name='login'),
 ]
+
