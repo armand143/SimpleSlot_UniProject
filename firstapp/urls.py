dff5ba63a4c035419df756005b0b1b1e14314ba6
armand143 <armandborel@yahoo.fr>
2022-07-04 22:58:27 +0200

    get recent

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('respage/<int:user_id>', views.ResPage, name='ResPage'),
     path('update_slots/<str:slot_value>/<int:res_id>/<int:user_id>', views.update_slots, name='update_slots'),
     path('whole_day/<int:res_id>/<int:user_id>', views.whole_day, name='whole_day'),
-    path('deleteSlot/<int:reservation_id>/<str:slot_value>', views.deleteSlot, name='deleteSlot')
+    path('deleteSlot/<int:reservation_id>/<str:slot_value>', views.deleteSlot, name='deleteSlot'),
      path('respageadmin/<int:cluster_id>', views.ResPageAdmin, name='ResPageAdmin')
 
 
