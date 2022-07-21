7833e5c098ef337391376f2207a415161a5a54f9
user0dave <david.s.hoermann@gmail.com>
2022-07-21 20:24:40 +0200

    settings database

--- todo_server/settings.py
+++ todo_server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
       
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sksydatabase',
+        'NAME': 'simpledatabase',
         'USER': 'postgres',
         'PASSWORD': 'sksypassword',
         'HOST': 'localhost',
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -139,3 +140,4 @@ EMAIL_HOST_PASSWORD = 'ftwuznfxfjhzavdt'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
